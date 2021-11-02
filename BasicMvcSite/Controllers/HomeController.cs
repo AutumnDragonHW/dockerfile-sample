40f4394e57efc841f3befc918d54e19889e532d0
Hendry Willy <hendry.willy@gmail.com>
2021-11-02 10:45:09 +0700

    set config for different variables for BasicMvcSite

--- BasicMvcSite/Controllers/HomeController.cs
+++ BasicMvcSite/Controllers/HomeController.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BasicMvcSite.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace BasicMvcSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IConfiguration config)
         {
             _logger = logger;
+            this.config = config;
         }
 
         public IActionResult Index()
         {
+            ViewData["EnvironmentIdentifier"] = config.GetValue<string>("EnvironmentIdentifier");
             return View();
         }
 
