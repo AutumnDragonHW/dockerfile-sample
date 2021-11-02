bb3c79889482ce7d2ff128b6bc2856462101f93c
Hendry Willy <hendry.willy@gmail.com>
2021-11-02 10:58:18 +0700

    add golang sample

---
+++ BasicGolangApi/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	// "encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+)
+
+/*
+	All Go programs start running from a function called `main` in a package called `main`
+*/
+func main() {
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/home/", getHome)
+	router.Run("localhost:8080")
+}
+
+func getHome(c *gin.Context) {
+	data := service.GetAllArtists()
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Called successfully"})
+}
